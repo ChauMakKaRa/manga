011506a212b469d812bff256731876c72e840b98
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2023-09-30 11:59:54 +0700

    homeControllers

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,44 @@
+const tableProduct = require('../models/product');
 
-const getHomeController = (req, res) => {
-    res.send('hello world new!!');
-}
+
+const getHomeController = async (req, res, next) => {
+    const products = 
+    await tableProduct.find({})
+        .then(products => res.render('home',{products}))
+        .catch(error => next(error));
+};
+
 
 const getHello = (req, res) => {
     res.render('sample')
 }
+
+const postLogin = (req, res, next) => {
+    res.render('acount/login');
+}
+
+const postRegister = (req, res, next) => {
+    res.render('acount/register');
+}
+
+const getListProducts = (req, res, next) => {
+    res.render('product/list');
+}
+
+const getContact = (req, res, next) => {
+    res.render('contact/contact');
+}
+
+const getCart = (req, res, next) => {
+    res.render('Cart/cart');
+}
+
 module.exports = {
-    getHomeController, getHello
+    getHello, 
+    getHomeController, 
+    postLogin, 
+    postRegister,
+    getListProducts,
+    getContact,
+    getCart,
 }
